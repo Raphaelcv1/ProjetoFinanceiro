42f0f75a54f23185ffc9fb3a1fd048fe731608b6
Raphaelcv1 <raphaelcv1@hotmail.com>
2021-02-25 00:04:11 -0300

    Aula8

--- MyFinance/src/app/recebidos/recebidos.page.ts
+++ MyFinance/src/app/recebidos/recebidos.page.ts
@@ -2,17 +2,18 @@ import { ModalController } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
 import { FormComponent } from './form/form.component';
 
-
 @Component({
   selector: 'app-recebidos',
   templateUrl: './recebidos.page.html',
   styleUrls: ['./recebidos.page.scss'],
 })
 export class RecebidosPage implements OnInit {
+  listaLancamentos = [];
 
   constructor(public modalController: ModalController) { }
 
   ngOnInit() {
+
   }
 
   async chamarFormulario() {
